9edbc4ae051450ccbcc77f4d1ab48fd07c5ee0f3
Sum-20 <131290708+Sum-20@users.noreply.github.com>
2023-04-19 23:06:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ num_2= st.number_input('Insert a num_2')
 num_3 = st.number_input('Insert a num_3')
 if (num_1 > num_2) and (num_1 > num_3):
    largest = num_1
-elif (num_2 > num1) and (num_2 > num_3):
+elif (num_2 > num_1) and (num_2 > num_3):
    largest = num_2
 else:
    largest = num_3
