30b6fc52c1e9f509e5c01a3c0e36383905228d72
Sum-20 <131290708+Sum-20@users.noreply.github.com>
2023-04-19 23:01:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 def largest_num(num_1,num_2,num_3):
-   num_1= st.number_input('Insert a number')
-   num_2= st.number_input('Insert a number')
-   num_3 = st.number_input('Insert a number')
    if (num_1 > num_2) and (num_1 > num_3):
      largest = num_1
    elif (num_2 > num1) and (num_2 > num_3):
@@ -10,4 +7,7 @@ def largest_num(num_1,num_2,num_3):
    else:
      largest = num_3
    return largest
+num_1= st.number_input('Insert a number')
+num_2= st.number_input('Insert a number')
+num_3 = st.number_input('Insert a number')
 st.write("The largest number is", largest_num)
